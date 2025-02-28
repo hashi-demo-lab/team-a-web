18d4f4e59264e6a91848415681c1b835fb5faa99
Aaron Evans <aarone@outlook.com.au>
2025-02-28 16:09:50 +1100

    comment out unused Terraform data sources and locals

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-data "tfe_outputs" "foo" {
-  organization = "hashi-demos-apj"
-  workspace    = "team-a-ec2"
-}
+# data "tfe_outputs" "foo" {
+#   organization = "hashi-demos-apj"
+#   workspace    = "team-a-ec2"
+# }
 
-data "vault_kv_secret_v2" "secret_role" {
-  mount = "demo-key-value"
-  name  = "aarons-secrets"
-}
+# data "vault_kv_secret_v2" "secret_role" {
+#   mount = "demo-key-value"
+#   name  = "aarons-secrets"
+# }
 
-locals {
-  vault_data = data.vault_kv_secret_v2.secret_role
-}
+# locals {
+#   vault_data = data.vault_kv_secret_v2.secret_role
+# }
 
 resource "aws_ssm_document" "web-script-document" {
   name            = "vault_script_document_${var.deployment_id}"
